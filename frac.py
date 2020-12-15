f905619bb1fdfd4a89f3420bba414890f79e4e09
Alexdieu <alexdieuveult@gmail.com>
2020-12-15 11:52:17 +0100

    Update frac.py

--- frac.py
+++ frac.py
@@ -1,7 +1,7 @@
 ## VERY poor code by alexdieu
-n = input("Note:")
-v = input("%s sur :" % n)
-g = input("Mettre %s/%s sur combien (15/25 = 12/20 donc je reponds 20 si je veux ca)?\n"% (n,v))
+n = input("Grade:")
+v = input("%s on :" % n)
+g = input("Put %s/%s on how much (15/25 = 12/20 so i answer 20 if i want that)?\n"% (n,v))
 n = float(n)
 v = float(v)
 g = float(g)
